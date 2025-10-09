8e29b017f82580a14d82d269fc8a45884667d85f
linyis2222 <linyis2222@gmail.com>
2025-10-09 11:43:26 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+const demons = [
+  { rank 1, name Thinking Space II, publisher CairoX },
+  { rank 2, name Amethyst, publisher iMist },
+  { rank 3, name Tidal Wave, publisher OniLink },
+];
+
+const listDiv = document.getElementById(list);
+demons.forEach(d = {
+  const card = document.createElement(div);
+  card.className = card;
+  card.innerHTML = `h2#${d.rank} - ${d.name}h2pby ${d.creator}p`;
+  listDiv.appendChild(card);
+});
