63bfad0d1a5fb8afa162112685023b152c501b79
linyis2222 <linyis2222@gmail.com>
2025-10-09 11:50:50 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 const demons = [
-  { rank 1, name Thinking Space II, publisher CairoX },
-  { rank 2, name Amethyst, publisher iMist },
-  { rank 3, name Tidal Wave, publisher OniLink },
+  { rank: 1, name: "Thinking Space II", publisher: "CairoX" },
+  { rank: 2, name: "Amethyst", publisher: "iMist" },
+  { rank: 3, name: "Tidal Wave", publisher: "OniLink" }
 ];
 
-const listDiv = document.getElementById(list);
-demons.forEach(d = {
-  const card = document.createElement(div);
-  card.className = card;
-  card.innerHTML = `h2#${d.rank} - ${d.name}h2pby ${d.creator}p`;
+const listDiv = document.getElementById("list");
+
+demons.forEach(d => {
+  const card = document.createElement("div");
+  card.className = "card";
+  card.innerHTML = `<h2>#${d.rank} - ${d.name}</h2><p>by ${d.publisher}</p>`;
   listDiv.appendChild(card);
 });
