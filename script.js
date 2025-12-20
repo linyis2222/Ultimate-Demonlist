417719463f4c0a0dcc16841467b484df5370a863
Linyi <linyis2222@gmail.com>
2025-12-20 20:01:59 +0900

    Update script.js

--- script.js
+++ script.js
@@ -15,7 +15,8 @@ const demons = [
     publisher: "iMist",
     video: "https://www.youtube.com/embed/4lfkzz1VCbA",
     id: "119550490",
-    password: "Free to Copy"
+    password: "Free to Copy",
+    tag: "Former Top1, Wave"
   },
   {
     name: "Flamewall",
@@ -3396,6 +3397,7 @@ document.addEventListener('DOMContentLoaded', () => {
         searchInput.addEventListener('input', () => buildLeftList());
     }
 });
+
 
 
 
