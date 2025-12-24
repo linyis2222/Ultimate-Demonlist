3593c3a5650ea60dd85e98fd300575b3aa18bee2
Linyi <linyis2222@gmail.com>
2025-12-24 20:49:39 +0900

    Update script.js

--- script.js
+++ script.js
@@ -241,6 +241,16 @@ const demons = [
     id: "113599729",
     password: "Free to Copy"
   },
+    {
+    name: "Based After Based [Unrated]",
+    creators: "Bedrock, Etherial, Dshifter, DashyGTC, Harsh, PastelPink, Eyeless Jack, Akunakunn",
+    verifier: "DiamondSplash",
+    publisher: "Akunakunn",
+    video: "https://www.youtube.com/embed/yQBFyUvB3lY",
+    id: "110534288",
+    password: "Free to Copy",
+    tag: "Unrated, Duals, Timings, Learny"
+  },
   {
     name: "The Lightning Rod",
     creators: "Lavatrex",
@@ -267,16 +277,6 @@ const demons = [
     video: "https://www.youtube.com/embed/hZ8vFX8z_BU",
     id: "109439644",
     password: "Free to Copy"
-  },
-    {
-    name: "Based After Based [Unrated]",
-    creators: "Bedrock, Etherial, Dshifter, DashyGTC, Harsh, PastelPink, Eyeless Jack, Akunakunn",
-    verifier: "DiamondSplash",
-    publisher: "Akunakunn",
-    video: "https://www.youtube.com/embed/yQBFyUvB3lY",
-    id: "110534288",
-    password: "Free to Copy",
-    tag: "Unrated, Duals, Timings, Learny"
   },
   {
     name: "Silentlocked",
@@ -3169,7 +3169,7 @@ const demons = [
    - 날짜는 문자열(YYYY-MM-DD 또는 로컬 포맷)로 표기.
    =========================== */
 const changeLog = [
-  { date: '2025-12-24', detail: 'Based After Based has been placed at #29, above Silentlocked and below KOSETSU. This pushes Jigsaw into the Extended List, and ta1LSD0ll into the Legacy List.' },
+  { date: '2025-12-24', detail: 'Based After Based has been placed at #26, above The Lightning Rod and below Subterminal Point. This pushes Jigsaw into the Extended List, and ta1LSD0ll into the Legacy List.' },
   { date: '2025-12-24', detail: 'Ashley Wave Trials has been placed at #19, above Tunnel of Despair and below Abyss of Darkness. This pushes Checked Steam into the Extended List, and Scrubbabingo force into the Legacy List.' },
   { date: '2025-12-24', detail: 'Aquamarine has been placed at #13, above Menace and below Anathema. This pushes Cimmerian Shade into the Extended List, and Call Me Maybe into the Legacy List.' },
   { date: '2025-12-24', detail: 'Graceful has been placed at #88, above The Paroxysm of Rage and below limbo but uwu ig idk. This pushes SAND SAILOR into the Legacy List.' },
@@ -3480,6 +3480,7 @@ document.addEventListener('DOMContentLoaded', () => {
         searchInput.addEventListener('input', () => buildLeftList());
     }
 });
+
 
 
 
