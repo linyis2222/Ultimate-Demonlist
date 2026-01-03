e66f20fce142f0ee435bf585f4346851ce75c3e8
Linyi <linyis2222@gmail.com>
2026-01-03 16:55:07 +0900

    Update script.js

--- script.js
+++ script.js
@@ -581,6 +581,16 @@ const demons = [
     password: "Free to Copy"
   },
   {
+    name: "Infinite Chaos",
+    creators: "CairoX, HelpegasuS, TriplasH, creoa, dutchiee, Renn241, Genoxide, DreamZoneGD, gruzy, Valentine, Mercury6779, Xenith, Insxne97, PlaEnterprise, Epdjip, humbjumb, william, Zephlin, AvaxLumino, Zronk",
+    verifier: "Victurkey",
+    publisher: "CairoX",
+    video: "https://www.youtube.com/embed/U6e6DD32noM",
+    id: "119689515",
+    password: "Free to Copy",
+    tag: "Fast-Paced, Learny"
+  },
+  {
     name: "Sinister Silence",
     creators: "Wahffle & more(101 people)",
     verifier: "wwdonut",
@@ -3646,6 +3656,7 @@ const demons = [
    - 날짜는 문자열(YYYY-MM-DD 또는 로컬 포맷)로 표기.
    =========================== */
 const changeLog = [
+  { date: '2026-01-03', detail: 'Infinite Chaos has been placed at #61, above Sinister Silence and below Mayhem. This pushes Crystal Crusher into the Extended List, and Neon Skyline into the Legacy List.' },
   { date: '2026-01-03', detail: 'Hard Machine has been raised from #120 to #117, above DISSONANCE and below Guideless Goobering.' },
   { date: '2026-01-01', detail: 'Calamity has been raised from #260 to #258, above Aquatic Auroras and below Shutdown.' },
   { date: '2026-01-01', detail: 'Happy new year everyone!' },
@@ -3972,6 +3983,7 @@ document.addEventListener('DOMContentLoaded', () => {
         searchInput.addEventListener('input', () => buildLeftList());
     }
 });
+
 
 
 
